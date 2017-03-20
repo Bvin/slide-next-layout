e61789894a2545e6a666f793530566a5e9c9310a
bvin <bvin@live.cn>
2017-03-20 17:33:10 +0800

    初步调通

--- app/src/main/java/cn/bvin/android/apps/demo/next/MainActivity.java
+++ app/src/main/java/cn/bvin/android/apps/demo/next/MainActivity.java
@@ -2,6 +2,9 @@ package cn.bvin.android.apps.demo.next;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import cn.bvin.android.lib.widget.next.SlideNextLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SlideNextLayout snl = (SlideNextLayout) findViewById(R.id.snl);
+        /*snl.setOnSlideCallback(new SlideNextLayout.OnSlideCallback() {
+            @Override
+            public boolean onSlideToNext(MotionEvent ev) {
+                return false;
+            }
+
+            @Override
+            public boolean onSlideToPrevious(MotionEvent ev) {
+                return false;
+            }
+        });*/
     }
 }
--- slidenext/src/main/java/cn/bvin/android/lib/widget/next/SlideNextLayout.java
+++ slidenext/src/main/java/cn/bvin/android/lib/widget/next/SlideNextLayout.java
@@ -3,10 +3,14 @@ package cn.bvin.android.lib.widget.next;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Scroller;
 
 /**
  * Created by bvin on 2017/3/15.
@@ -14,7 +18,7 @@ import android.widget.LinearLayout;
 
 public class SlideNextLayout extends LinearLayout {
 
-    private boolean mEnable;
+    private boolean mEnable = true;
 
     private View mFirstPage;
     private View mNextPage;
@@ -24,12 +28,18 @@ public class SlideNextLayout extends LinearLayout {
     private int mLastY;
     private int mLastInterceptY;
 
+    private OnSlideCallback mOnSlideCallback;
+
+    private int mCurrentPage = 0;// 默认在第一页
+    private Scroller mScroller;
+
     public SlideNextLayout(Context context) {
         super(context);
     }
 
     public SlideNextLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mScroller = new Scroller(context);
         // 最小有效移动距离
         mTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
     }
@@ -48,8 +58,28 @@ public class SlideNextLayout extends LinearLayout {
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        if (hasWindowFocus) {
+            ViewGroup.LayoutParams lps = mNextPage.getLayoutParams();
+            if (lps.height != mFirstPage.getMeasuredHeight()) {
+                lps.height = mFirstPage.getMeasuredHeight();
+                mNextPage.setLayoutParams(lps);
+            }
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (mFirstPage != null) {
+            Log.d("onFinishInflate: ", "" + mFirstPage.getMeasuredHeight());
+        }
+    }
+
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (!mEnable) return super.onInterceptTouchEvent(ev);
+        if (!mEnable || mFirstPage == null || mNextPage == null) return super.onInterceptTouchEvent(ev);
         final int y = (int) ev.getY();
         switch (ev.getAction()){
             case MotionEvent.ACTION_DOWN:
@@ -57,10 +87,23 @@ public class SlideNextLayout extends LinearLayout {
                 break;
             case MotionEvent.ACTION_MOVE:
                 int movedDistance = y - mLastInterceptY;
-                if (movedDistance > mTouchSlop/*&&第一页*/) {// 往下移动
-
-                } else if (movedDistance < -mTouchSlop/*&&第二页*/) {// 往上移动
-
+                if (movedDistance > mTouchSlop && mCurrentPage == 1) {
+                    if (mOnSlideCallback != null) {
+                        return mOnSlideCallback.onSlideToPrevious(ev);
+                    }else {
+                        if (mNextPage instanceof ScrollView){
+                            return mNextPage.getScrollY() == 0;
+                        }
+                    }
+                } else if (movedDistance < -mTouchSlop && mCurrentPage == 0) {
+                    if (mOnSlideCallback != null) {
+                        return mOnSlideCallback.onSlideToNext(ev);
+                    }else {
+                        if (mFirstPage instanceof ScrollView){
+                            return mFirstPage.getScrollY() + mFirstPage.getHeight() >= ((ScrollView) mFirstPage).getChildAt(0)
+                                    .getHeight();
+                        }
+                    }
                 }
                 break;
             case MotionEvent.ACTION_UP:
@@ -69,5 +112,72 @@ public class SlideNextLayout extends LinearLayout {
                 break;
         }
         return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dy = y - mLastY;
+                if (mCurrentPage == 0){
+                    int sy = -dy;
+                    if (sy < 0) {
+                        sy = 0;
+                    } else if (sy > getHeight()) {
+                        sy = getHeight();
+                    }
+                    scrollTo(0, sy);
+                }else if(mCurrentPage == 1){
+                    if (dy > 0) {
+                        scrollTo(0, mFirstPage.getMeasuredHeight() - dy);
+                    } else { // dy < 0
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                int mHeightOfFirstPage = mFirstPage.getMeasuredHeight();
+                int t = 0;
+                switch (mCurrentPage) {
+                    case 0:
+                        t = mHeightOfFirstPage / 4;
+                        break;
+                    case 1:
+                        t = mHeightOfFirstPage * 3 / 4;
+                        break;
+                }
+                int sy = getScrollY();
+                if (sy > t) { // scroll to footer
+                    mScroller.startScroll(0, sy, 0, mHeightOfFirstPage - sy, 150);
+                    mCurrentPage = 1;
+                    invalidate();
+                } else { // scroll to header
+                    mScroller.startScroll(0, sy, 0, -sy, 150);
+                    mCurrentPage = 0;
+                    invalidate();
+                }
+                mLastY = 0;
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    public void setOnSlideCallback(OnSlideCallback onSlideCallback) {
+        mOnSlideCallback = onSlideCallback;
+    }
+
+    public interface OnSlideCallback{
+        boolean onSlideToNext(MotionEvent ev);
+        boolean onSlideToPrevious(MotionEvent ev);
     }
 }
